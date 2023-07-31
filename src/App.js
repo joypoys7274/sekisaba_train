550b9452f411fba0457c40d29715a3237e57379a
JOY <supercomputerjoi@gmail.com>
2023-07-31 23:47:14 +0900

    Create initial web pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>関鯖wiki</h1>
+
+      <h2>日数カウンタ</h2>
+      
+      <ul>
+        <li><a href="http://sekisaba.mydns.jp:8123/">map</a></li>
+        <li><a href="#">mod一覧ページ(準備中)</a></li>
+        <li><a href="#">駅探ナビ(準備中)</a></li>
+      </ul>
+
     </div>
   );
 }
