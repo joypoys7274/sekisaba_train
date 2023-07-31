d7eda8805bbaa2c23b033598ff8fc16a055170b0
JOY <supercomputerjoi@gmail.com>
2023-08-01 00:03:38 +0900

    Develop Day Counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import DayCounter from './DayCounter';
 
 function App() {
   return (
     <div className="App">
       <h1>関鯖wiki</h1>
 
-      <h2>日数カウンタ</h2>
-      
+      <DayCounter />
+
       <ul>
         <li><a href="http://sekisaba.mydns.jp:8123/">map</a></li>
         <li><a href="#">mod一覧ページ(準備中)</a></li>
---
+++ src/DayCounter.js
@@ -0,0 +1,18 @@
+import moment from "moment"
+
+class DayCounter extends React.Component {
+    constructor(props) {
+        super(props);
+        const dataFrom = moment("2023-08-01 00:00:00");
+        const today = moment();
+        this.state = {
+            days: dataFrom.diff(today, "days"),
+        };
+    }
+
+    render() {
+        return (
+            <h2>日数カウンタ：{this.state.days}日目</h2>
+        )
+    }
+}
